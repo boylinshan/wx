fcc89319dac6837d86daca4b93eda049e826fb49
boylinshan <boylinshan@sjtu.edu.cn>
2016-07-31 19:16:11 +0800

    update

--- Service/ServiceCosts.py
+++ Service/ServiceCosts.py
@@ -5,9 +5,9 @@ class ServiceCosts(Service):
 		super(ServiceCosts, self).__init__()
 
 	def parse(self, uid, content):
-		result = self.db.query('select money from costs where uid = "%s"' % uid)
+		result = self.database.query('select money from costs where uid = "%s"' % uid)
 		if not result:
-			self.db.insert('insert into costs values("%s", 300)' % uid)
+			self.database.insert('insert into costs values("%s", 300)' % uid)
 			return 300
 		else:
 			return 200
