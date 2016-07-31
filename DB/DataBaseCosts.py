eb1106d856e1fad438a2261ba0edf52c9952efd0
boylinshan <boylinshan@sjtu.edu.cn>
2016-07-31 22:56:01 +0800

    update

--- DB/DataBaseCosts.py
+++ DB/DataBaseCosts.py
@@ -2,8 +2,10 @@ from DataBase import DataBase
 
 class DataBaseCosts(DataBase):
 	def __init__(self):
-		print '**********'
 		super(DataBaseCosts, self).__init__()
+
+	def query(self):
+		print '****'
 
 
 
