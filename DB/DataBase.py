1b5bbbc17409fc95af05aef5dce588842f776216
boylinshan <boylinshan@sjtu.edu.cn>
2016-07-31 22:36:56 +0800

    update

--- DB/DataBase.py
+++ DB/DataBase.py
@@ -4,21 +4,18 @@ from utils.tools import Singleton
 def OperateWrap(operate):
 	def operateWrap(self, param):
 		self.conn = sqlite3.connect(self.database)
-		return operate(self, param)
-		#self.conn.commit()
-		#self.conn.close()
+		operate(self, param)
+		self.conn.commit()
+		self.conn.close()
 
 	return operateWrap
-
 
 class DataBase(object):
 	def __init__(self):
 		super(DataBase, self).__init__()
 		self.database = self.__class__.__name__[7:] + '.db'
 		self.conn = None
-		print '**'
 		self.init()
-		print self
 
 	@OperateWrap
 	def init(self, sql=None):
--- Service/ServiceCosts.py
+++ Service/ServiceCosts.py
@@ -5,6 +5,7 @@ class ServiceCosts(Service):
 		super(ServiceCosts, self).__init__()
 
 	def parse(self, uid, content):
+		print self.database
 		result = self.database.query('select money from costs where uid = "%s"' % uid)
 		if not result:
 			self.database.insert('insert into costs values("%s", 300)' % uid)
