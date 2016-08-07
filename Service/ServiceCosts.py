6b10649707eb29279bcca45131e600ddeba849d8
boylinshan <boylinshan@sjtu.edu.cn>
2016-08-07 14:32:16 +0800

    update

--- Service/ServiceCosts.py
+++ Service/ServiceCosts.py
@@ -10,4 +10,8 @@ class ServiceCosts(Service):
 			self.database.insert('insert into costs values("%s", 300)' % uid)
 			return 300
 		else:
+			print result
+			print type(result)
+			print content
+			print type(content)
 			return 200
